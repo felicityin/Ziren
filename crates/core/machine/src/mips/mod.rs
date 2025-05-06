4a4a86b775b96c1a3d5fe65cd941da45ea57cc42
Jingping Yin <yinjingping2022@gmail.com>
2025-05-06 15:39:14 +0800

    fix: keccak rows per event

--- crates/core/machine/src/mips/mod.rs
+++ crates/core/machine/src/mips/mod.rs
@@ -1,9 +1,8 @@
 use core::fmt;
 use itertools::Itertools;
 use zkm_core_executor::{
-    events::PrecompileLocalMemory, syscalls::SyscallCode, ExecutionRecord, MipsAirId, Program,
+    events::{PrecompileLocalMemory, SyscallEvent}, syscalls::SyscallCode, ExecutionRecord, MipsAirId, Program,
 };
-
 use crate::{
     global::GlobalChip,
     memory::{MemoryChipType, MemoryLocalChip, NUM_LOCAL_MEMORY_ENTRIES_PER_ROW},
@@ -63,6 +62,8 @@ pub const MAX_LOG_NUMBER_OF_SHARDS: usize = 16;
 
 /// The maximum number of shards in core.
 pub const MAX_NUMBER_OF_SHARDS: usize = 1 << MAX_LOG_NUMBER_OF_SHARDS;
+
+const KECCAK_ROWS_PER_BLOCK: usize = 24;
 
 /// An AIR for encoding MIPS execution.
 ///
@@ -465,20 +466,7 @@ impl<F: PrimeField32> MipsAir<F> {
             .get_events(self.syscall_code())
             .filter(|events| !events.is_empty())
             .map(|events| {
-                let mut num_rows = events.len() * self.rows_per_event();
-                if self.syscall_code() == SyscallCode::KECCAK_SPONGE {
-                    num_rows = events
-                        .iter()
-                        .map(|(_, pre_e)| {
-                            if let PrecompileEvent::KeccakSponge(event) = pre_e {
-                                event.num_blocks() * 24
-                            } else {
-                                unreachable!()
-                            }
-                        })
-                        .sum::<usize>();
-                }
-
+                let num_rows = events.len() * self.rows_per_event(Some(record));
                 (
                     num_rows,
                     events.get_local_mem_events().into_iter().count(),
@@ -563,12 +551,40 @@ impl<F: PrimeField32> MipsAir<F> {
             .collect()
     }
 
-    pub(crate) fn rows_per_event(&self) -> usize {
+    pub(crate) fn rows_per_event(&self, record: Option<&ExecutionRecord>) -> usize {
         match self {
             Self::Sha256Compress(_) => 80,
             Self::Sha256Extend(_) => 48,
+            Self::KeccakSponge(_) => self.keccak_rows_per_event(record),
             _ => 1,
         }
+    }
+
+    fn keccak_rows_per_event(&self, record: Option<&ExecutionRecord>) -> usize {
+        if let Some(record) = record {
+            record
+                .precompile_events
+                .get_events(SyscallCode::KECCAK_SPONGE)
+                .filter(|events| !events.is_empty())
+                .map(|events| {
+                    self.calculate_keccak_rows(&events)
+                }).unwrap_or(KECCAK_ROWS_PER_BLOCK)
+        } else {
+            KECCAK_ROWS_PER_BLOCK
+        }
+    }
+
+    fn calculate_keccak_rows(&self, events: &Vec<(SyscallEvent, PrecompileEvent)>) -> usize {
+        events
+            .iter()
+            .map(|(_, pre_e)| {
+                if let PrecompileEvent::KeccakSponge(event) = pre_e {
+                    event.num_blocks() *  KECCAK_ROWS_PER_BLOCK
+                } else {
+                    unreachable!()
+                }
+            })
+            .sum::<usize>()
     }
 
     pub(crate) fn syscall_code(&self) -> SyscallCode {
--- crates/core/machine/src/shape/mod.rs
+++ crates/core/machine/src/shape/mod.rs
@@ -207,6 +207,7 @@ impl<F: PrimeField32> CoreShapeConfig<F> {
                             air,
                             *memory_events_per_row,
                             *allowed_log2_height,
+                            Some(record),
                         ) {
                             let mem_events_height = shape[2].1;
                             let global_events_height = shape[3].1;
@@ -240,9 +241,10 @@ impl<F: PrimeField32> CoreShapeConfig<F> {
         air: &MipsAir<F>,
         memory_events_per_row: usize,
         allowed_log2_height: usize,
+        record: Option<&ExecutionRecord>,
     ) -> Vec<[(String, usize); 4]> {
         // TODO: This is a temporary fix to the shape, concretely fix this
-        (1..=4 * air.rows_per_event())
+        (1..=4 * air.rows_per_event(record))
             .rev()
             .map(|rows_per_event| {
                 let num_local_mem_events =
@@ -252,7 +254,7 @@ impl<F: PrimeField32> CoreShapeConfig<F> {
                     (
                         MipsAir::<F>::SyscallPrecompile(SyscallChip::precompile()).name(),
                         ((1 << allowed_log2_height)
-                            .div_ceil(&air.rows_per_event())
+                            .div_ceil(&air.rows_per_event(record))
                             .next_power_of_two()
                             .ilog2() as usize)
                             .max(4),
@@ -268,9 +270,9 @@ impl<F: PrimeField32> CoreShapeConfig<F> {
                     (
                         MipsAir::<F>::Global(GlobalChip).name(),
                         ((2 * num_local_mem_events
-                            + (1 << allowed_log2_height).div_ceil(&air.rows_per_event()))
-                        .next_power_of_two()
-                        .ilog2() as usize)
+                            + (1 << allowed_log2_height).div_ceil(&air.rows_per_event(record)))
+                            .next_power_of_two()
+                            .ilog2() as usize)
                             .max(4),
                     ),
                 ]
@@ -312,7 +314,7 @@ impl<F: PrimeField32> CoreShapeConfig<F> {
         let precompile_only_shapes = self.partial_precompile_shapes.iter().flat_map(
             move |(air, (mem_events_per_row, allowed_log_heights))| {
                 allowed_log_heights.iter().flat_map(move |allowed_log_height| {
-                    self.get_precompile_shapes(air, *mem_events_per_row, *allowed_log_height)
+                    self.get_precompile_shapes(air, *mem_events_per_row, *allowed_log_height, None)
                 })
             },
         );
@@ -399,6 +401,7 @@ impl<F: PrimeField32> CoreShapeConfig<F> {
                     air,
                     *mem_events_per_row,
                     *allowed_log_heights.last().unwrap(),
+                    None,
                 )
             },
         );
