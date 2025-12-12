0d42b14bca57ae1210822662c98811669d21d756
Jingping Yin <yinjingping2022@gmail.com>
2025-12-12 16:20:55 +0800

    test: change public values of compressed proof

--- examples/aggregation/host/src/main.rs
+++ examples/aggregation/host/src/main.rs
@@ -1,9 +1,20 @@
 //! A simple example showing how to aggregate proofs of multiple programs with ZKM.
 
+use std::borrow::BorrowMut;
+use std::collections::BTreeMap;
+
 use zkm_sdk::{
     include_elf, HashableKey, ProverClient, ZKMProof, ZKMProofWithPublicValues, ZKMStdin,
     ZKMVerifyingKey,
 };
+use zkm_recursion_circuit::{
+    // config::InnerSC,
+    merkle_tree::MerkleTree,
+};
+use zkm_recursion_core::air::RecursionPublicValues;
+use zkm_stark::DIGEST_SIZE;
+use zkm_prover::InnerSC;
+use p3_koala_bear::KoalaBear;
 
 /// A program that aggregates the proofs of the simple program.
 const AGGREGATION_ELF: &[u8] = include_elf!("aggregation");
@@ -23,6 +34,11 @@ fn main() {
     // Setup the logger.
     zkm_sdk::utils::setup_logger();
 
+    pub static VK_MAP: &'static [u8] = include_bytes!("../vk_map.bin");
+    let allowed_vk_map: BTreeMap<[KoalaBear; DIGEST_SIZE], usize> =
+        bincode::deserialize(&VK_MAP).unwrap();
+    let (recursion_vk_root, _merkle_tree) =  MerkleTree::<KoalaBear, InnerSC>::commit(allowed_vk_map.keys().copied().collect());
+
     // Initialize the proving client.
     let client = ProverClient::new();
 
@@ -30,7 +46,19 @@ fn main() {
     let (aggregation_pk, _) = client.setup(AGGREGATION_ELF);
 
     // Generate the fibonacci proofs.
-    let proof_1 = ZKMProofWithPublicValues::load("proof-with-pvs.bin").expect("loading proof failed");
+    let mut proof_1 = ZKMProofWithPublicValues::load("proof-with-pvs.bin").expect("loading proof failed");
+
+    let ZKMProof::Compressed(mut proof) = proof_1.proof else { panic!() };
+    let mut public_values: &mut RecursionPublicValues<_> =
+        proof.proof.public_values.as_mut_slice().borrow_mut();
+    public_values.vk_root = recursion_vk_root;
+
+    proof_1 = ZKMProofWithPublicValues {
+        proof: ZKMProof::Compressed(proof),
+        public_values: proof_1.public_values,
+        zkm_version: "v1.2.3".to_string(),
+    };
+
     let fibonacci_vk: zkm_sdk::ZKMVerifyingKey =
         bincode::deserialize(&std::fs::read("vk.bin").unwrap()).unwrap();
 
