4c5b7712b6db64bd79ca906c94f955030323caaa
Jingping Yin <yinjingping2022@gmail.com>
2026-02-26 18:29:03 +0800

    chore: reth example

---
+++ examples/reth/host/src/main.rs
@@ -0,0 +1,16 @@
+use zkm_sdk::{include_elf, utils, ProverClient, ZKMProofWithPublicValues, ZKMStdin};
+
+pub const RETH_ELF: &[u8] = include_bytes!("../../guest/reth");
+pub const RETH_STDIN: &[u8] = include_bytes!("../../guest/24438200-stdin.bin");
+
+fn main() {
+    // Setup a tracer for logging.
+    utils::setup_logger();
+
+    let stdin: ZKMStdin = bincode::deserialize(RETH_STDIN).unwrap();
+
+    let client = ProverClient::new();
+    client.execute(RETH_ELF, stdin).run().unwrap();
+
+    println!("successfully!")
+}
