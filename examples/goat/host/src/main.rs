a90d57cf6f0f60ae53888877899194f488ba647a
Jingping Yin <yinjingping2022@gmail.com>
2026-07-22 18:46:51 +0800

    chore(example): goat

---
+++ examples/goat/host/src/main.rs
@@ -0,0 +1,24 @@
+use zkm_sdk::{utils, ProverClient, ZKMStdin};
+
+pub const GOAT_ELF: &[u8] = include_bytes!("../../guest/goat");
+pub const GOAT_STDIN: &[u8] = include_bytes!("../../guest/stdin");
+
+fn main() {
+    // Setup a tracer for logging.
+    utils::setup_logger();
+
+    let stdin: ZKMStdin = bincode::deserialize(GOAT_STDIN).unwrap();
+
+    let client = ProverClient::new();
+    client.execute(GOAT_ELF, &stdin).run().unwrap();
+    println!("execute successfully!");
+
+     // Generate the proof for the given guest and input.
+    let (pk, vk) = client.setup(GOAT_ELF);
+    let proof = client.prove(&pk, stdin).run().unwrap();
+    println!("generated proof");
+
+    // Verify proof and public values
+    client.verify(&proof, &vk).expect("verification failed");
+    println!("verify successfully!");
+}
